53d014d5cc4e26c9427d897b19cf98cd03d3f3c2
jinyong <zhaqh146@naver.com>
2022-05-06 18:03:46 +0900

    2022-05-06

--- 2579 - 계단 오르기.py
+++ 2579 - 계단 오르기.py
@@ -1,35 +1,31 @@
-def solution(idx, result):
-    '''
-    idx : 다음 번 밟은 계단의 번호, -1부터 시작
-    result : 밟은 계단의 점수 합
-    만약 visited 에 번호가 연속한 3개가 나오게 되면 해당 계단은 밟지 않는다
-    '''
-    global answer
-
-    if idx >= 2:
-        if visited[idx] == 1 and visited[idx-1] == 1 and visited[idx-2] == 1:
-            return
-
-    if idx == N-1:  # 마지막 계단을 밟은 경우
-        if result > answer:
-            answer = result
-        return
-
-    visited[idx+1] = 1
-    solution(idx+1, result+stairs[idx+1])
-    visited[idx+1] = 0
-
-    if idx + 2 >= N:
-        return
-
-    visited[idx+2] = 1
-    solution(idx+2, result+stairs[idx+2])
-    visited[idx+2] = 0
-
-
 N = int(input())
-stairs = [int(input()) for _ in range(N)]
-answer = 0
-visited = [0] * N
-solution(-1, 0)
-print(answer)
+
+stairs = [0] + [int(input()) for _ in range(N)]
+
+numbers = [[0] * 3 for i in range(N + 1)]
+# 맨 처음 지점은 계단으로 취급하지 않기 때문에 1칸 + 1칸 식으로 올라갈 수 있다
+# 그래서 첫 번째 계단을 다 밟았다고 해놓고 시작해야 한다
+numbers[1] = [stairs[1]] * 3
+
+
+for i in range(2, N + 1):
+    """
+    1칸+2칸
+    2칸+1칸
+    2칸+2칸
+    이렇게 3가지 경우로 나눠서 생각해보자
+    """
+
+    # 1칸 + 2칸 상태가 되려면
+    # 2칸 전이 2칸 + 1칸 이어야 한다
+    numbers[i][0] = stairs[i] + numbers[i-2][1]
+
+    # 2칸 + 1칸 상태가 되려면
+    # 이전 칸까지 1칸 + 2칸, 2칸 + 2칸 이어야 한다
+    numbers[i][1] = stairs[i] + max(numbers[i-1][0], numbers[i-1][2])
+
+    # 2칸 + 2칸 상태가 되려면
+    # 2칸 전이 1칸 + 2칸, 2칸 + 2칸 이어야 한다
+    numbers[i][2] = stairs[i] + max(numbers[i-2][0], numbers[i-2][2])
+
+print(max(numbers[N]))
